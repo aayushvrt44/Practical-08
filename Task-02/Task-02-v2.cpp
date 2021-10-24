e082cd7bdf407bf29b35de65f1cf2d6ec863b480
aayushvrt44 <91362918+aayushvrt44@users.noreply.github.com>
2021-10-24 09:38:05 +0530

    Add files via upload

---
+++ Task-02/Task-02-v2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main()
+{
+  int arr[] = {10,20,30};
+  int *ptr = arr;
+  
+  cout << *ptr << endl;
+  ptr++;
+  cout << *ptr << endl;
+  return 0;
+}
